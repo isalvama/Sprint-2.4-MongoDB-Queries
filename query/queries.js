e8561344e7b240cfbcfa3879a71c61c4f81dc658
isalvama <inesalvans@gmail.com>
2026-03-26 09:56:44 +0100

    Add queries 1 and 2 to fetch restaurant data

--- query/queries.js
+++ query/queries.js
@@ -1,8 +1,8 @@
 // 1. Mostrar tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find()
 
 // 2. Mostrar el restaurant_id i name per tots els documents en la col·lecció Restaurants.
-
+db.restaurants.find({restaurant_id: 1, name: 1})
 
 // 3. Mostrar el restaurant_id, name, borough i cuisine.
 
