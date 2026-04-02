cc25d8a64a8caf180fd4dda3c110ed7af02ed09d
isalvama <inesalvans@gmail.com>
2026-04-02 13:50:02 +0200

    feat: add cuisine, grades.score, name and address.street indexes to restaurants collection for a more efficient search when running queries

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,5 @@
 db.restaurants.createIndex({borough: 1})
+db.restaurants.createIndex({cuisine: 1})
+db.restaurants.createIndex({grades.score: 1})
+db.restaurants.createIndex({name: 1})
+db.restaurants.createIndex({address.street: 1})
