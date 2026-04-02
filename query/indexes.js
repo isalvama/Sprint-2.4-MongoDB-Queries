3d60374e19635bd40d698538891b1b0510002325
isalvama <inesalvans@gmail.com>
2026-04-02 18:29:52 +0200

    feat: add new indexes "grades.1.score" and "location.coordinates" for best performance in restaurant queries

--- query/indexes.js
+++ query/indexes.js
@@ -6,6 +6,10 @@ db.restaurants.createIndex({ name: 1 })
 
 db.restaurants.createIndex({"location": "2dsphere"})
 
+db.restaurants.createIndex({"location.coordinates": 1})
+
 db.restaurants.createIndex({ "grades.score": 1 })
+
+db.restaurants.createIndex({ "grades.1.score": 1 })
 
 db.restaurants.createIndex({ "address.street": 1 })
