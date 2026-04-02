9fc424f01315cc4855496685f98bd8b75d1b90da
isalvama <inesalvans@gmail.com>
2026-04-02 16:13:21 +0200

    fix: add format to createIndex function

--- query/indexes.js
+++ query/indexes.js
@@ -1,1 +1,7 @@
-db.restaurants.createIndex({borough: 1, cuisine: 1, grades.score: 1, name: 1, address.street: 1})
+db.restaurants.createIndex({
+  "borough": 1, 
+  "cuisine": 1, 
+  "grades.score": 1, 
+  "name": 1, 
+  "address.street": 1
+})
